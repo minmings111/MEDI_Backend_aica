ad27e704345cd886c63de0192962d46920e11afa
minjeong <minmings111@gmail.com>
2025-10-17 13:15:59 +0900

    first commit

---
+++ src/test/java/com/medi/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medi.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
